934a2dba3728bccab4f06069d1d7289cfa9c7431
Robin <carlos.oli.h23@gmail.com>
2024-07-23 14:14:08 -0300

    Commit final com desafio concluído.

--- src/Contador.java
+++ src/Contador.java
@@ -11,8 +11,22 @@ public class Contador {
         System.out.println("Digite o segundo número: ");
         int numeroDois = scanner.nextInt();
 
-        int soma = numeroUm+numeroDois;
+        try {
+            comparar(numeroUm, numeroDois);
 
-        System.out.println(soma);
+        }catch (ParametrosInvalidosException exception) {
+            System.err.println("O primeiro número deve ser menor que o segundo número.");
+        }
+        }
+
+    static void comparar(int numeroUm, int numeroDois) throws ParametrosInvalidosException{
+        if(numeroUm > numeroDois)
+            throw new ParametrosInvalidosException();
+
+        int conta = numeroDois - numeroUm;
+        for (int x = 1; x <= conta; x++) {
+            System.out.println("Imprimindo o número " + x);
+        }
     }
+
 }
